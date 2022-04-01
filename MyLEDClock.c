4d284c5295c4d0c9b16d04a3291519a4a23e0feb
Rizwan Ahmed <35874979+Riz1ahmed@users.noreply.github.com>
2022-04-02 05:31:19 +0600

    Off LED glowing issue fixed

--- MyLEDClock.c
+++ MyLEDClock.c
@@ -1,38 +1,36 @@
 #include<Wire.h>
 #include<DS3231.h>
+
 DS3231 rtc(SDA, SCL);
 Time t;
 
-bool glowSec=false;
 int milliSec=0;
 
-int gnd=2, v5=3;
+int gnd=2, v5=3; //Register Power.
 int dataPin=4,latchPin=5, clockPin=6;
-int g1=7; //4 no segment gnd
-int g2=8; //3 no segment gnd
-int g3=9; //second segment gnd
-int g4=10;//2 no segment gnd
-int g5=11;//1 no segment gnd
 
+int dGnds[]={7,8,9,10,11};//From Left to Right
 int digits[]={
  //gfedcba
-  B0111111,//63
-  B0110000,//48
-  B1101101,//109
-  B1111001,//121
-  B1110010,//114
-  B1011011,//91
-  B1011111,//95
-  B0110001,//49
-  B1111111,//127
-  B1111011, //123
-  B0000001 //1=For second
+  B00111111,//0=63
+  B00110000,//1=48
+  B01101101,//2=109
+  B01111001,//3=121
+  B01110010,//4=114
+  B01011011,//5=91
+  B01011111,//6=95
+  B00110001,//7=49
+  B01111111,//8=127
+  B01111011,//9=123
+  B00000001 //:=For second
 };
 
 void setup() {
   rtc.begin();
-  //Serial.begin(9600);
-  
+  Serial.begin(9600);
+
+  //setTime(); //For set time
+
   pinMode(gnd,OUTPUT),digitalWrite(gnd,LOW);
   pinMode(v5,OUTPUT),digitalWrite(v5,HIGH);
   
@@ -40,61 +38,49 @@ void setup() {
   pinMode(latchPin,OUTPUT);
   pinMode(clockPin,OUTPUT);
 
-  pinMode(g1,OUTPUT),digitalWrite(g1,HIGH);
-  pinMode(g2,OUTPUT),digitalWrite(g2,HIGH);
-  pinMode(g3,OUTPUT),digitalWrite(g3,HIGH);
-  pinMode(g4,OUTPUT),digitalWrite(g4,HIGH);
-  pinMode(g5,OUTPUT),digitalWrite(g5,HIGH);
+  pinMode(dGnds[0],OUTPUT),digitalWrite(dGnds[0],HIGH);
+  pinMode(dGnds[1],OUTPUT),digitalWrite(dGnds[1],HIGH);
+  pinMode(dGnds[2],OUTPUT),digitalWrite(dGnds[2],HIGH);
+  pinMode(dGnds[3],OUTPUT),digitalWrite(dGnds[3],HIGH);
+  pinMode(dGnds[4],OUTPUT),digitalWrite(dGnds[4],HIGH);
 }
-
-int i=1;
 void loop() {
-  milliSec>1000 ? milliSec=0 : milliSec++;
+  milliSec>1000 ? milliSec=0 : milliSec+=5;
   
   t = rtc.getTime();
+  if (milliSec==950) Serial.print(t.sec, DEC);
   showTime();
 }
 
 void showTime(){
-   
    int m=t.min;
-   showDigit(g5, digits[m%10]);
-   showDigit(g1, digits[m/10]);
-
    int h=t.hour%12;
    if (!h) h=12;
-   if (milliSec>500){
-     showDigit(g2,digits[10]);
-     showDigit(g3, digits[h%10]);
-   }else showDigit(g2, digits[h%10]);
-   showDigit(g4, digits[h/10]);
-    
-  //Serial.println(milliSec);
+   //Minute
+   showDigit(dGnds[0], digits[m%10]);
+   showDigit(dGnds[1], digits[m/10]);
+   //Blinking Second
+   if (milliSec>500) showDigit(dGnds[2],digits[10]);
+   //Houre
+   showDigit(dGnds[3],digits[h%10]);
+   showDigit(dGnds[4], digits[h/10]);
 }
 
-void showDigit(int dg, int digitValue){
-  digitalWrite(dg,0);
+void showDigit(int digitGnd, int digitValue){
+  //ResetGnd
+  for (int i=0; i<5; i++) digitalWrite(dGnds[i], 1);
+  //Digit High
   digitalWrite(latchPin, 0);
   shiftOut(dataPin,clockPin, LSBFIRST, digitValue);
   digitalWrite(latchPin, 1);
-  digitalWrite(dg,1);
-}
-
-void blinkSecond(){
-  if (milliSec>500) glowSec=!glowSec, milliSec=0;
-  milliSec++;
-  if (glowSec) showDigit(g3,1);
-}
-
-void blinkLED(int x){
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin,clockPin, LSBFIRST, x);
-  digitalWrite(latchPin, HIGH);
+  //Selected segment Gnd High, So that, Show digit.
+  digitalWrite(digitGnd, 0);
+  delayMicroseconds(1000);//Stay digit for 1 millisecond.
 }
 
 void setTime(){
-  //For set time please uncomment these lines.
-   rtc.setDOW(SATURDAY);
-   rtc.setTime(04,21,00);//hh,mm,ss
-   rtc.setDate(03,11,2021); //dd,MM,yyyy
+  //For set time please call this function from setup.
+   rtc.setDOW(SATURDAY);//Day name
+   rtc.setTime(12,55,00);//hh,mm,ss
+   rtc.setDate(03,03,2022); //dd,MM,yyyy
 }
